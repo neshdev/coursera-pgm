164491ff9da6a1a670b68e03afe475014636ca86
Dhinesh Devanathan <Dhinesh Devanathan>
2016-11-07 22:05:37 -0500

    working version of ComputeMarginals

--- PA-Simple-BN-Knowledge-Engineering-Release/ComputeMarginal.m
+++ PA-Simple-BN-Knowledge-Engineering-Release/ComputeMarginal.m
@@ -27,7 +27,10 @@ end
 % Remember to renormalize the entries of M!
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-M = struct('var', [], 'card', [], 'val', []); % Returns empty factor. Change this.
+  M = ComputeJointDistribution(F);
+  M = ObserveEvidence(M, E);
+  M.val = M.val / sum(M.val);
+  M = FactorMarginalization(M,  setdiff(M.var, V));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 end
